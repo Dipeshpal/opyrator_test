c9f8e276b2fc706425c5e81d6ba4113c6f3492a1
Dipesh Paul <dipeshpal17@gmail.com>
2021-07-21 18:19:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ def generate_text(input: TextGenerationInput) -> TextGenerationOutput:
     return TextGenerationOutput(generated_text=res[0]["generated_text"])
 
 
-#if __name__ == '__main__':
-#    generate_text()
+if __name__ == '__main__':
+    generate_text
